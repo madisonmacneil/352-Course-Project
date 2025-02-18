b69b1564bfad5d5055099a4fd555d3490a4ea335
madisonmacneil <121136189+madisonmacneil@users.noreply.github.com>
2025-02-17 23:58:08 -0500

    Initializations

---
+++ deep_learning/course_scraper.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+#https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/
+#https://www.queensu.ca/academic-calendar/business/bachelor-commerce/courses-of-instruction/by20number/
+
+
