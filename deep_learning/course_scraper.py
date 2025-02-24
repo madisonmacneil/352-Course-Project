67261e1e470b6439f204fb4deee165a437434b4e
madisonmacneil <121136189+madisonmacneil@users.noreply.github.com>
2025-02-24 14:10:16 -0500

    Course Scraper Finalized

--- deep_learning/course_scraper.py
+++ deep_learning/course_scraper.py
@@ -3,22 +3,21 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import csv 
 import pandas as pd 
+import os 
 
-#https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/
-#https://www.queensu.ca/academic-calendar/business/bachelor-commerce/courses-of-instruction/by20number/
-
-def program_urls():
+def program_urls(path):
     driver = webdriver.Chrome()
-    driver.get("https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/")
+    driver.get(path)
 
     course_links = []
     links = driver.find_elements(By.CSS_SELECTOR, ".sitemap li a")  
     for li in links:
         course_links.append(li.get_attribute("href"))
 
-    with open("links.csv", 'w') as myfile: 
+    with open("links.csv", 'a') as myfile: 
         wr= csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(course_links)
+        for link in course_links:
+            wr.writerow([link])
 
     driver.quit()
 
@@ -27,20 +26,43 @@ def program_courses(url):
     driver.get(url)
     course_info_collection = [] 
     courses = driver.find_elements(By.CSS_SELECTOR, ".courseblock")
+    requirements = None
     for course in courses: 
         course_info = [] 
+
         #columns = course code, course name, units, description, requirements, offering faculty, learning outcomes 
         course_code = course.find_element(By.CSS_SELECTOR, ".detail-code").text  
         course_name = course.find_element(By.CSS_SELECTOR, ".detail-title").text 
         units = course.find_element(By.CSS_SELECTOR, ".detail-hours_html").text 
         description = course.find_element(By.CSS_SELECTOR, ".courseblockextra").text
-        requirements = course.find_element(By.CSS_SELECTOR, ".detail-requirements").text
+        try: requirements = course.find_element(By.CSS_SELECTOR, ".detail-requirements").text
+        except: 
+            pass
         faculty = course.find_element(By.CSS_SELECTOR, ".detail-offering_faculty").text
         outcomes = course.find_element(By.CSS_SELECTOR, ".detail-cim_los").text
-
+        
         course_info.extend([course_code, course_name, units, description, requirements, faculty, outcomes])
         course_info_collection.append(course_info)
-    print(course_info_collection)
+
+    df = pd.DataFrame(course_info_collection, columns = ['course_code', 'course_name', 'units', 'description', 'requirements', 'faculty', 'outcomes'])
+    output_path = 'db.csv'
+    df.to_csv(output_path, mode ='a', header = not os.path.exists(output_path))
     driver.quit()
 
-program_courses("https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/anat/")
+
+def courses_url_by_faculty(faculty_urls):
+    for i in range(len(faculty_urls)): 
+        program_urls(faculty_urls[i])
+
+def course_info_scrape(my_file): 
+    with open(my_file, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader: 
+            print(row[0])
+            program_courses(row[0])
+
+#courses_url_by_faculty(['https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/', 'https://www.queensu.ca/academic-calendar/engineering-applied-sciences/courses-instruction/'])
+
+course_info_scrape('links.csv')
+
+#"https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/chem/"
---
+++ deep_learning/model.py
@@ -0,0 +1,5 @@
+#idea flow: translate input into the query --> bag of words ?? 
+
+#https://www.reddit.com/r/SQL/comments/my9hjk/is_this_project_doable_nlp_based_query_system_for/ 
+
+
