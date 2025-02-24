7245969e9f09fabe3a6ad537384a917f8b67ec1e
madisonmacneil <121136189+madisonmacneil@users.noreply.github.com>
2025-02-24 16:41:39 -0500

    DB!!!!!

--- deep_learning/db_preprocessing.py
+++ deep_learning/db_preprocessing.py
@@ -4,9 +4,9 @@ df = pd.read_csv('db.csv')
 print(df.head(5))
 
 #df has 24920 observations 
-print(f"Size before removing duplicates: {df.size}")
+print(f"Size before removing duplicates: {df.shape}")
 df = df.drop_duplicates()
-print(f"Size after removing duplicates: {df.size}")
+print(f"Size after removing duplicates: {df.shape}")
 
 #df after removing duplicates has 22920 observations 
 
